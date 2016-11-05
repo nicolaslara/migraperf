283c4e802e7c989d1fd217ec84abda01172f5d16
Yann Malet <yann.malet@gmail.com>
2016-11-05 14:52:01 +0100

    Add the inheritchains app

--- create_many_migrations.sh
+++ create_many_migrations.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 for i in `seq $1`
 do
-cp deep/models.tmpl.1 deep/models.py
-./manage.py makemigrations deep
-cp deep/models.tmpl.2 deep/models.py
-./manage.py makemigrations deep
+cp inheritchains/models.tmpl.1 inheritchains/models.py
+time ./manage.py makemigrations
+cp inheritchains/models.tmpl.2 inheritchains/models.py
+time ./manage.py makemigrations
 done
